64acb00283b21cf6b4b6e0ada912b10218c0ff88
chetan.dobariya <chetan.dobariya89@gmail.com>
2024-08-25 11:00:12 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Chetankumar Dobariya on 25.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
